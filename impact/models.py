14fb510bc6f7c4c7a0e980cd4275e86fe7ef3d0c
rramees <ramees8614@gmail.com>
2024-01-09 11:18:24 +0530

    Initial commit

---
+++ impact/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class Product:
+    id : str
+    prod_name : str
+    desc : str
+    image : str
---
+++ impact/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Product
+
+# Create your views here.
+
+def index(request):
+
+    prod1 = Product()
+    prod1.prod_name = 'Iphone'
+    prod1.desc = 'All new Iphone 15 pro Max'
+    prod1.image = 'app-1.jpg'
+
+    prod2 = Product()
+    prod2.prod_name = 'Apple Watch'
+    prod2.desc = 'All new Apple Watch'
+    prod2.image = 'product-1.jpg'
+
+    prod3 = Product()
+    prod3.prod_name = 'Face cream pack'
+    prod3.desc = 'Clear your pimples with the all new face cream pack'
+    prod3.image = 'branding-1.jpg'
+
+    products = [prod1 , prod2 , prod3 ]
+    
+    return render(request, 'index.html' ,{'products': products})
